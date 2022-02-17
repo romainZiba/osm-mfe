8135089bb09e230d827131d62561cab6c30debcb
Romain Ziba <romain.ziba@gmail.com>
2022-02-17 09:04:36 +0100

    add Angular Material

--- apps/osm-shell/src/app/app.module.ts
+++ apps/osm-shell/src/app/app.module.ts
@@ -1,8 +1,9 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,6 +19,7 @@ import {AppComponent} from './app.component';
       ],
       { initialNavigation: 'enabledBlocking' }
     ),
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
